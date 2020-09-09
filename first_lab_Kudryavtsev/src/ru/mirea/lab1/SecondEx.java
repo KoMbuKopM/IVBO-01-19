be8d351cfe863fe4cb76e02a7f725910a49a20db
Kudryavtsev D E <70705351+KoMbuKopM@users.noreply.github.com>
2020-09-09 17:00:01 +0300

    Add files via upload

---
+++ first_lab_Kudryavtsev/src/ru/mirea/lab1/SecondEx.java
@@ -0,0 +1,13 @@
+package ru.mirea.lab1;
+
+import java.util.Scanner;
+
+public class SecondEx {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        String line;
+        for (int i = 0; i< args.length; i++){
+            System.out.print(args[i] + " ");
+        }
+    }
+}
